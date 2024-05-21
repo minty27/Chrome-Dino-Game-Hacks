3642626982331ca2a22933e9478fa8dd70c7d035
Not Owen <168646530+minty27@users.noreply.github.com>
2024-05-21 16:07:24 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// Enter the line below first into the console
+// You can set the speed to whatever you like
+Runner.instance_.setSpeed(100)
+
+// Enter this line below afterwards
+Runner().tRex.groundYPos = 10
