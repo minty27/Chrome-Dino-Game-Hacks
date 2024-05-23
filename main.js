35c96890c07a1dc3d615d0992c55b4332fd0f2dc
Not Owen <168646530+minty27@users.noreply.github.com>
2024-05-23 07:37:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// Enter the line below first into the console
-// You can set the speed to whatever you like
-Runner.instance_.setSpeed(100)
+// Enter the line below first into the console  
+Runner().tRex.groundYPos = 10
 
 // Enter this line below afterwards
-Runner().tRex.groundYPos = 10
+// If you want to set the speed higher, do it in intervals. Start with 100, then 500, the 1000
+Runner.instance_.setSpeed(100)
